12f4690b46597650083abfae42a94ea5ad121bde
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-06-14 13:38:18 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import axios from 'axios'
 import './App.css'
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import SearchBar from "./components/SearchBar/SearchBar"
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+import { fetchImages } from './api';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState("");
+  const [modalAlt, setModalAlt] = useState("");
+  const [images, setImages] = useState([]);
 
+
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const handleOpenModal = (imgUrl, imgAlt) => {
+    setShowModal(true);
+    setModalUrl(imgUrl);
+    setModalAlt(imgAlt);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  useEffect(() => {
+    if (query === "") {
+      return;
+    }
+
+
+    useEffect(() => {
+      async function getImages() {
+        try {
+          setError(false);
+          setIsLoading(true);
+          const data = await fetchImages(query, page);
+          setShowBtn(data.total_pages && data.total_pages !== page);
+          setImages((prevImages) => {
+            return [...prevImages, ...data.results];
+          });
+        } catch (error) {
+          setError(true);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getImages();
+    }, [query, page]);
+
+
+    ;
+  }, []);
   return (
     <>
-      <div>
+      <SearchBar onSubmit={handleSearch} />
+      <div className={css.container} >
+        {isLoading && <Loader />}
+        {error && <ErrorMessage />}
+        {images.length > 0 && <ImageGallery items={images} onOpen={handleOpenModal} />}
+        {images.length > 0 && !isLoading && showBtn && <LoadMoreBtn onClik={handleLoadMore} />}
+        {images.length > 0 && (
+          <ImageModal modal={showModal} onClose={handleCloseModal} url={modalUrl} alt={modalAlt} />
+        )}
 
       </div>
 
